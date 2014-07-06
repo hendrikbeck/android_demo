d24021a85a3e09d2f2e7c63f9b1582bd3a2f24fe
Hendrik Beck <hendrik.beck@gmail.com>
2014-07-06 16:01:55 +0200

    Added main activity to list all example screens; moved checkout demo onto separate screen; added 'fake' checkout screen contents (though not very pretty yet); upgraded to SDK 0.0.2

--- app/src/main/java/com/hendrikbeck/murmio_demo/app/MainActivity.java
+++ app/src/main/java/com/hendrikbeck/murmio_demo/app/MainActivity.java
@@ -1,57 +1,23 @@
 package com.hendrikbeck.murmio_demo.app;
 
-import android.os.StrictMode;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RatingBar;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.BasicHttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 import io.murmio.android.sdk.Murm;
 
-public class MainActivity extends ActionBarActivity  {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Murm m = (Murm) findViewById(R.id.murm_view_example);
-        m.setUserId("My demo app test user");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void openCheckDemoActivity(View view) {
+        Intent intent = new Intent(this, CheckoutDemoActivity.class);
+        startActivity(intent);
     }
 
 }
