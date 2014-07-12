3a42d421a05cd37e3427ff2b91c23dfcb4ee32a8
Hendrik Beck <hendrik.beck@gmail.com>
2014-07-12 11:45:50 +0200

    Updated app to SDK 0.0.3

--- app/src/main/java/com/hendrikbeck/murmio_demo/app/CheckoutDemoActivity.java
+++ app/src/main/java/com/hendrikbeck/murmio_demo/app/CheckoutDemoActivity.java
@@ -1,15 +1,13 @@
 package com.hendrikbeck.murmio_demo.app;
 
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 
 import io.murmio.android.sdk.Murm;
 
-public class CheckoutDemoActivity extends ActionBarActivity  {
+public class CheckoutDemoActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +19,12 @@ public class CheckoutDemoActivity extends ActionBarActivity  {
         m.setUserId("Hendrik Beck");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
